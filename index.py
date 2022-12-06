94c112af80c9fcb221ee072ecb5d652c35bd52b5
soso-saos <119938772+soso-saos@users.noreply.github.com>
2022-12-06 12:55:24 +0100

    sdsfs

--- index.py
+++ index.py
@@ -1,1 +1,15 @@
-print("hey")
+x=1
+while x==1:
+    print("0-quitter")
+    print("1-écrire dans le repertoire")
+    print("2-Rechercher dan sle répertoire")
+    reponse=input("Votre choix ?")
+    if reponse=="0":
+        x=0
+        print("Au revoir!")
+    elif reponse=="1":
+        x=2
+        print("Nom(0 pour terminer")
+    
+        
+        
